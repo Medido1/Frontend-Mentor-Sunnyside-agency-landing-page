bfd7d8538274ef93ea493ed365bccf5a73abc538
Medido1 <medi1222@gmail.com>
2024-04-10 08:14:27 +0100

    Create Main and benefits components
      create main components which will have the main content of the document
      and benefits components in the main

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import benefitArr from '../data/benefitsData';
+import '../styles/benefits.css';
+
+export default function Benefits() {
+  return (
+    <ul className="benefits">
+      {benefitArr.map((benefit) => (
+        <li key={benefit.id}>
+          <img
+            src={window.innerWidth < 768 ? benefit.srcMobile : benefit.srcDestop}
+            alt={benefit.alt}
+            className="benefit_img"
+          />
+          <div className="benefit_content">
+            <h2>{benefit.title}</h2>
+            <p>{benefit.description}</p>
+            <button className="btn btn_more"
+              style={{ borderBottom: benefit.color }}
+            >
+              Learn more
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Benefits from './Benefits';
+
+export default function Main() {
+  return (
+    <main className="main">
+      <Benefits />
+    </main>
+  );
+}
