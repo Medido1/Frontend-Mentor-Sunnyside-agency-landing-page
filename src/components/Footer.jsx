520dcb39d7061c89f26c26cb53dfa30f10d07e71
Medido1 <medi1222@gmail.com>
2024-04-10 09:16:38 +0100

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import facebook from '../assets/images/icon-facebook.svg';
+import instagram from '../assets/images/icon-instagram.svg';
+import twitter from '../assets/images/icon-twitter.svg';
+import pinterset from '../assets/images/icon-pinterest.svg';
+import '../styles/footer.css';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <h2>sunnyside</h2>
+      <ul className="nav">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Projects</a></li>
+      </ul>
+      <ul className="social_links">
+        <a href="#"><img src={facebook} alt="faceook link" className="icon social" /></a>
+        <a href="#"><img src={instagram} alt="instagram link" className="icon social" /></a>
+        <a href="#"><img src={twitter} alt="twitter link" className="icon social" /></a>
+        <a href="#"><img src={pinterset} alt="pineterest link" className="icon social" /></a>
+      </ul>
+    </footer>
+  );
+}
